b9f7b3a5bf7365691138271f4023eed7a7462b5b
Chace <727661040@qq.com>
2022-02-07 23:28:18 +0800

    修改properties

--- src/test/java/com/hqc/sms/SmsApplicationTests.java
+++ src/test/java/com/hqc/sms/SmsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.hqc.sms;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class SmsApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getConnection());
+        System.out.println(dataSource.getClass());
     }
 
 }
